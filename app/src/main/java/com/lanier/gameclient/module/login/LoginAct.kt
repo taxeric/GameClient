0da27cab9a86e251464e444b333632b4c087d734
幻弦让叶 <1248389474@qq.com>
2024-02-03 22:57:13 +0800

    新增登录页面

---
+++ app/src/main/java/com/lanier/gameclient/module/login/LoginAct.kt
@@ -0,0 +1,12 @@
+package com.lanier.gameclient.module.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lanier.gameclient.R
+
+class LoginAct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
