e88cb7995cef3368daa7c1081d0bc82578bb74e1
幻弦让叶 <1248389474@qq.com>
2024-04-06 14:41:24 +0800

    processing the growth time of plants

--- app/src/main/java/com/lanier/gameclient/entity/Land.kt
+++ app/src/main/java/com/lanier/gameclient/entity/Land.kt
@@ -16,15 +16,17 @@ data class Land(
     val cropId: Int = -1,
     val seed: Seed? = null, // 当前种植的作物
     val status: Int = 0, // 土地状态, 1=未解锁, 2=种植中, 3=空闲
-    val currentStage: Int = 0, // 作物当前所处的阶段
+    var currentStage: Int = 0, // 作物当前所处的阶段
     val maxStage: Int = 0, // 最大阶段
-    val nextStageAllTime: String = "", // 下一阶段开始时间点, json字符串
-    val nextStageStartTime: Long = 0L, // 下一阶段的开始时间, 单位ms
-    val nextStageRemainTime: Int = 0, // 到下一阶段剩余时间, 单位s
+    var nextStageAllTime: String = "", // 下一阶段开始时间点, json字符串
+    var nextStageStartTime: Long = 0L, // 下一阶段的开始时间, 单位ms
+    var nextStageRemainTime: Int = 0, // 到下一阶段剩余时间, 单位s
     val lastHarvestTime: Long = 0L, // 上一次收获时间
     val maxHarvestCount: Int = 0, // 作物最大可收获次数
     val harvestCount: Int = 0, // 作物已收获次数
 ) {
+
+    var calcCanHarvest = false
 
     fun seedStr() = if (locked()) "待解锁~" else seed?.name ?: "空闲中~"
 
@@ -32,7 +34,7 @@ data class Land(
 
     fun canHarvest(): Boolean {
         return status == PLANTING &&
-                currentStage == maxStage &&
+                currentStage + 1 == maxStage &&
                 nextStageRemainTime <= 0 &&
                 harvestCount < maxHarvestCount
     }
--- app/src/main/java/com/lanier/gameclient/module/plant/PlantVM.kt
+++ app/src/main/java/com/lanier/gameclient/module/plant/PlantVM.kt
@@ -2,17 +2,22 @@ package com.lanier.gameclient.module.plant
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.lanier.gameclient.base.BaseViewModel
+import com.lanier.gameclient.base.ViewStatus
 import com.lanier.gameclient.base.data.UserData
 import com.lanier.gameclient.entity.BaseListModel
 import com.lanier.gameclient.entity.Land
 import com.lanier.gameclient.entity.Backpack
+import com.lanier.gameclient.entity.LandCropStageInfo
 import com.lanier.gameclient.entity.SeedStageInfo
 import com.lanier.gameclient.entity.dto.PlantDto
 import com.lanier.gameclient.ext.launchSafe
 import com.lanier.gameclient.net.API
 import okhttp3.FormBody
+import kotlin.math.abs
 
 class PlantVM : BaseViewModel() {
 
@@ -37,18 +42,10 @@ class PlantVM : BaseViewModel() {
             val requestBody = FormBody.Builder()
                 .add("petId", UserData.curPet.petId.toString())
                 .build()
-            successGetCatch<BaseListModel<Land>>(API.LAND_INFO, requestBody)?.let {
-                it.list.forEach { land ->
-                    val jacksonMapper = jacksonObjectMapper()
-                    land.seed?.let { seed ->
-                        val ssi = try {
-                            jacksonMapper.readValue(seed.stageInfo, SeedStageInfo::class.java)
-                        } catch (e: Exception) {
-                            null
-                        }
-                        seed.seedStageInfo = ssi
-                    }
-                }
+            setStatus(ViewStatus.Loading)
+            successGetCatch<BaseListModel<Land>>(API.LAND_INFO, requestBody, showLoading = false)?.let {
+                processLandPlants(it.list)
+                setStatus(ViewStatus.Completed)
                 _landInfos.postValue(it.list)
             }
         }
@@ -126,5 +123,114 @@ class PlantVM : BaseViewModel() {
                 _backpackList.postValue(Pair(it.hasNext, it.list))
             }
         }
+    }
+
+    private fun processLandPlants(lands: List<Land>) {
+        val jacksonMapper = jacksonObjectMapper()
+        lands.forEach { land ->
+            if (land.status == Land.PLANTING) {
+                refreshImpl(land)
+            }
+            land.seed?.let { seed ->
+                val ssi = try {
+                    jacksonMapper.readValue(seed.stageInfo, SeedStageInfo::class.java)
+                } catch (e: Exception) {
+                    null
+                }
+                seed.seedStageInfo = ssi
+            }
+        }
+    }
+
+    private fun refreshImpl(curLandInfo: Land) {
+        val objectMapper = jacksonObjectMapper()
+        try {
+            // 当前阶段值
+            var currentStage: Int = curLandInfo.currentStage
+            val nextStageStartTime: Long
+            val nextStageRemainTime: Int
+            val canHarvest: Boolean
+            val nextStageAllTime: String = curLandInfo.nextStageAllTime
+            val cropStageTimeInfo = objectMapper.readValue(nextStageAllTime, LandCropStageInfo::class.java)
+            val calcStage = calculateStage(
+                cropStageTimeInfo.nextStageAllTime,
+                currentStage
+            )
+            currentStage = calcStage.realCurrentStage
+            nextStageStartTime = calcStage.realNextStageStartTime
+            nextStageRemainTime = calcStage.realNextStageRemainTime
+            canHarvest = calcStage.canHarvest
+            curLandInfo.currentStage = currentStage
+            curLandInfo.nextStageStartTime = nextStageStartTime
+            curLandInfo.nextStageRemainTime = nextStageRemainTime
+            curLandInfo.calcCanHarvest = canHarvest
+        } catch (e: JsonProcessingException) {
+            println(">>>> process refresh failed ${e.message}")
+        }
+    }
+
+    /**
+     * 计算当前时间对应的阶段和时间
+     *
+     * @param allNextStageTime 下个阶段开始时间节点
+     * @param currentStage     当前阶段index
+     * @return 实际的阶段, 实际的开始时间, 实际的剩余时间, 是否可以收获
+     */
+    private fun calculateStage(
+        allNextStageTime: List<Long>,
+        currentStage: Int
+    ): CalcStage {
+        var canHarvest = false // 已经过了最终阶段, 可以收获了
+        val curTime = System.currentTimeMillis() // 获取当前时间
+        var realCurrentStage = 0
+        var realNextStageStartTime = 0L
+        var realNextStageRemainTime = 0
+        val lastStageTime = allNextStageTime[allNextStageTime.size - 1]
+        if (curTime < lastStageTime) { // 说明还未处于可收获阶段
+            for (i in currentStage until allNextStageTime.size) {
+                val nextStageTime = allNextStageTime[i] // 下一阶段的开始时间戳
+                if (curTime < nextStageTime) { // 就处于当前阶段
+                    realCurrentStage = i
+                    realNextStageStartTime = nextStageTime
+                    val remainTimeToNextStage = abs(((curTime - nextStageTime) / 1000).toDouble())
+                        .toInt() // 当前时间到下一阶段的剩余秒数
+                    realNextStageRemainTime = remainTimeToNextStage
+                    break
+                }
+            }
+        } else {
+            canHarvest = true
+            realCurrentStage = allNextStageTime.size - 1
+            realNextStageStartTime = 0
+            realNextStageRemainTime = 0
+        }
+        val calcStage = CalcStage()
+        calcStage.realCurrentStage = realCurrentStage
+        calcStage.realNextStageStartTime = realNextStageStartTime
+        calcStage.realNextStageRemainTime = realNextStageRemainTime
+        calcStage.canHarvest = canHarvest
+        return calcStage
+    }
+
+    private class CalcStage {
+        /**
+         * 真实的阶段
+         */
+        var realCurrentStage: Int = 0
+
+        /**
+         * 真实的下阶段开始时间
+         */
+        var realNextStageStartTime: Long = 0
+
+        /**
+         * 真实的距离下阶段的剩余时间
+         */
+        var realNextStageRemainTime: Int = 0
+
+        /**
+         * 是否可以收获
+         */
+        var canHarvest: Boolean = false
     }
 }
